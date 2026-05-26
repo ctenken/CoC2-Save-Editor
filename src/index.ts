ce1ec1391474611cbe69941fbee094ebef723528
ctenken <ctenken@gmail.com>
2026-05-26 16:09:30 -0500

    Version bump - 0.8.35

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { displayDisclaimer } from "./UI/Disclaimer";
 import "./Window";
 
 const editorVersion = "69";
-const gameVersion = "0.8.31";
+const gameVersion = "0.8.35";
 const lastBreakingVersion = "62";
 
 document.addEventListener("DOMContentLoaded", () => {
